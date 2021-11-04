7a062b8b8372cf17cc98e324fe3a37632de08a6d
sinkbaek <jeppest2@gmail.com>
2021-11-04 16:36:00 +0100

    plotting related commit

--- Binary_Analysis/plot_results.py
+++ Binary_Analysis/plot_results.py
@@ -4,11 +4,35 @@ import numpy as np
 
 matplotlib.rcParams.update({'font.size': 18})
 
+# # # asteroseismic estimates # # #
+m_rodrigues = np.array([1.517793, 0.06921])     # dnu_ref 134.9
+m_rodrigues_2 = np.array([1.4484853630502232, 0.06813140075559415])     # dnu_ref 134.9
+m_buldgen = np.array([1.23, 0.06])
+m_kjeldsen = np.array([1.68646277, 0.0740400])
+m_gaulm = np.array([1.52, 0.06])
+
+r_rodrigues = np.array([8.086049, 0.1455146])   # dnu_ref 134.9
+r_rodrigues_2 = np.array([7.96419, 0.146602])   # dnu_ref 134.98, numax=3141
+r_buldgen = np.array([7.45, 0.11])
+r_kjeldsen = np.array([8.5235094, 0.1441203])
+r_gaulm = np.array([8.1, 0.1])
+
+rho_rodrigues = np.array([0.002870804, 3.710783E-05])   # dnu_ref 134.9
+rho_rodrigues_2 = np.array([0.00286, 3.710783E-05])   # dnu_ref 134.98, numax=3141
+rho_buldgen = np.array([])
+rho_kjeldsen = np.array([0.002723463, 3.09484389E-05])
+rho_gaulme = np.array([0.00285, 0.00003])
+
+logg_rodrigues = np.array([2.803411, 0.00543766])   # dnu_ref 134.9
+logg_kjeldsen = np.array([2.80341117, 0.00543766])
+logg_gau_ast = np.array([2.802, 0.004])
+
 # # # Mass # # #
 fig = plt.figure(figsize=(16, 9))
 gs = fig.add_gridspec(1, 2)
 ax1 = fig.add_subplot(gs[0, 0])
 ax2 = fig.add_subplot(gs[0, 1])
+
 
 not_kepler = np.array([1.2544743268, 0.8137981554, 0.01453, 0.00521])  # val A, val B, err A, err B
 not_tess = np.array([1.254074808, 0.8131272818, 0.01451, 0.00522])
@@ -24,10 +48,28 @@ ax1.errorbar(2, gau_kepler[0], yerr=gau_kepler[2], fmt='p', color='orange', mark
 ax1.errorbar(3, not_tess[0], yerr=not_tess[2], fmt='bp', markersize=markersize)
 ax1.errorbar(4, gau_tess[0], yerr=gau_tess[2], fmt='gp', markersize=markersize)
 ax1.errorbar(5, gau_16_m[0], yerr=gau_16_m[2], fmt='p', color='indigo', markersize=markersize)
-ax1.plot([0.7, 5.20], [1.52, 1.52], '--', color='black', linewidth=1.0)
-ax1.plot([0.7, 5.20], [1.518, 1.518], '.-', color='red', linewidth=1.0)
-ax1.fill_between([0.7, 5.20], [1.52-0.06, 1.52-0.06], [1.52+0.06, 1.52+0.06], alpha=0.4, color='grey')
-ax1.fill_between([0.7, 5.20], [1.518-0.082, 1.518-0.082], [1.518+0.082, 1.518+0.082], alpha=0.4, color='indianred')
+ax1.plot([0.70, 5.2], [m_gaulm[0], m_gaulm[0]], '--', color='black', linewidth=1.0)
+ax1.plot([0.70, 5.2], [m_rodrigues[0], m_rodrigues[0]], '.-', color='red', linewidth=1.0)
+ax1.plot([0.70, 5.2], [m_buldgen[0], m_buldgen[0]], '.-', color='indigo', linewidth=1.0)
+ax1.fill_between(
+    [0.7, 5.20],
+    [m_gaulm[0]-m_gaulm[1], m_gaulm[0]-m_gaulm[1]],
+    [m_gaulm[0]+m_gaulm[1], m_gaulm[0]+m_gaulm[1]],
+    alpha=0.4, color='grey'
+)
+ax1.fill_between(
+    [0.7, 5.20],
+    [m_rodrigues[0]-m_rodrigues[1], m_rodrigues[0]-m_rodrigues[1]],
+    [m_rodrigues[0]+m_rodrigues[1], m_rodrigues[0]+m_rodrigues[1]],
+    alpha=0.4, color='indianred'
+)
+ax1.fill_between(
+    [0.7, 5.20],
+    [m_buldgen[0]-m_buldgen[1], m_buldgen[0]-m_buldgen[1]],
+    [m_buldgen[0]+m_buldgen[1], m_buldgen[0]+m_buldgen[1]],
+    alpha=0.4, color='mediumpurple'
+)
+
 
 ax1.set_xticks([1, 2, 3, 4, 5])
 ax1.set_xticklabels(['NOT Kepler', 'Gaulme Kepler', 'NOT TESS', 'Gaulme TESS', 'Gaulme et al. 2016'], rotation=20)
@@ -72,10 +114,27 @@ ax1.errorbar(2, gau_kepler[0], yerr=gau_kepler[2], fmt='p', color='orange', mark
 ax1.errorbar(3, not_tess[0], yerr=not_tess[2], fmt='bp', markersize=markersize)
 ax1.errorbar(4, gau_tess[0], yerr=gau_tess[2], fmt='gp', markersize=markersize)
 ax1.errorbar(5, gau_16_r[0], yerr=gau_16_r[2], fmt='p', color='indigo', markersize=markersize)
-ax1.plot([0.80, 5.2], [8.1, 8.1], '--', color='black', linewidth=1.0)
-ax1.plot([0.80, 5.2], [8.086, 8.086], '.-', color='red', linewidth=1.0)
-ax1.fill_between([0.80, 5.20], [8.1-0.1, 8.1-0.1], [8.1+0.1, 8.1+0.1], alpha=0.4, color='grey')
-ax1.fill_between([0.80, 5.20], [8.086-0.1655, 8.086-0.1655], [8.086+0.1655, 8.086+0.1655], alpha=0.4, color='indianred')
+ax1.plot([0.80, 5.2], [r_gaulm[0], r_gaulm[0]], '--', color='black', linewidth=1.0)
+ax1.plot([0.80, 5.2], [r_rodrigues[0], r_rodrigues[0]], '.-', color='red', linewidth=1.0)
+ax1.plot([0.80, 5.2], [r_buldgen[0], r_buldgen[0]], '.-', color='indigo', linewidth=1.0)
+ax1.fill_between(
+    [0.7, 5.20],
+    [r_gaulm[0]-r_gaulm[1], r_gaulm[0]-r_gaulm[1]],
+    [r_gaulm[0]+r_gaulm[1], r_gaulm[0]+r_gaulm[1]],
+    alpha=0.4, color='grey'
+)
+ax1.fill_between(
+    [0.7, 5.20],
+    [r_rodrigues[0]-r_rodrigues[1], r_rodrigues[0]-r_rodrigues[1]],
+    [r_rodrigues[0]+r_rodrigues[1], r_rodrigues[0]+r_rodrigues[1]],
+    alpha=0.4, color='indianred'
+)
+ax1.fill_between(
+    [0.7, 5.20],
+    [r_buldgen[0]-r_buldgen[1], r_buldgen[0]-r_buldgen[1]],
+    [r_buldgen[0]+r_buldgen[1], r_buldgen[0]+r_buldgen[1]],
+    alpha=0.4, color='mediumpurple'
+)
 
 ax1.set_xticks([1, 2, 3, 4, 5])
 ax1.set_xticklabels(['NOT Kepler', 'Gaulme Kepler', 'NOT TESS', 'Gaulme TESS', 'Gaulme et al. 2016'], rotation=20)
@@ -117,10 +176,20 @@ ax1.errorbar(2, logg_gau_kepler[0], yerr=logg_gau_kepler[2], fmt='p', color='ora
 ax1.errorbar(3, logg_not_tess[0], yerr=logg_not_tess[2], fmt='bp', markersize=markersize)
 ax1.errorbar(4, logg_gau_tess[0], yerr=logg_gau_tess[2], fmt='gp', markersize=markersize)
 ax1.errorbar(5, logg_gau_16[0], yerr=logg_gau_16[2], fmt='p', color='indigo', markersize=markersize)
-ax1.plot([0.80, 5.2], [2.802, 2.802], '--', color='black', linewidth=1.0)
-ax1.plot([0.80, 5.2], [2.8034, 2.8034], '.-', color='red', linewidth=1.0)
-ax1.fill_between([0.80, 5.20], [2.802-0.004, 2.802-0.004], [2.802+0.004, 2.802+0.004], alpha=0.4, color='grey')
-ax1.fill_between([0.80, 5.20], [2.8034-0.0069, 2.8034-0.0069], [2.8034+0.0069, 2.8034+0.0069], alpha=0.4, color='indianred')
+ax1.plot([0.80, 5.2], [logg_gau_ast[0], logg_gau_ast[0]], '--', color='black', linewidth=1.0)
+ax1.plot([0.80, 5.2], [logg_rodrigues[0], logg_rodrigues[0]], '.-', color='red', linewidth=1.0)
+ax1.fill_between(
+    [0.7, 5.20],
+    [logg_gau_ast[0]-logg_gau_ast[1], logg_gau_ast[0]-logg_gau_ast[1]],
+    [logg_gau_ast[0]+logg_gau_ast[1], logg_gau_ast[0]+logg_gau_ast[1]],
+    alpha=0.4, color='grey'
+)
+ax1.fill_between(
+    [0.7, 5.20],
+    [logg_rodrigues[0]-logg_rodrigues[1], logg_rodrigues[0]-logg_rodrigues[1]],
+    [logg_rodrigues[0]+logg_rodrigues[1], logg_rodrigues[0]+logg_rodrigues[1]],
+    alpha=0.4, color='indianred'
+)
 
 ax1.set_xticks([1, 2, 3, 4, 5])
 ax1.set_xticklabels(['NOT Kepler', 'Gaulme Kepler', 'NOT TESS', 'Gaulme TESS', 'Gaulme et al. 2016'], rotation=20)
@@ -137,15 +206,10 @@ fig = plt.figure(figsize=(16, 9))
 gs = fig.add_gridspec(1, 1)
 ax1 = fig.add_subplot(gs[0, 0])
 
-# rho_rodrigues = np.array([0.003744297, 0.000043945]) #solar ref 134.9
-rho_rodrigues = np.array([0.0036785607558513134, 0.00022954244665584795])  # solar ref 136.1
-rho_kjeldsen = np.array([0.0035521212, 0.000035485])
-rho_gaulme = np.array([0.00285, 0.00003])
-
-rho_not_kepler = np.array([0.00293589, 0.0, 0.00006134, 0.0])
-rho_gau_kepler = np.array([0.00291372, 0.0, 0.00009851, 0.0])
-rho_not_tess   = np.array([0.00305849, 0.0, 0.00010652, 0.0])
-rho_gau_tess   = np.array([0.00307411, 0.0, 0.00016133, 0.0])
+rho_not_kepler = np.array([0.00293589, 0.0, 0.0000435819, 0.0])
+rho_gau_kepler = np.array([0.00291372, 0.0, 0.000038446, 0.0])
+rho_not_tess   = np.array([0.00305849, 0.0, 0.0000604796, 0.0])
+rho_gau_tess   = np.array([0.00307411, 0.0, 0.0000487894, 0.0])
 rho_gau_16     = np.array([0.00293, 0.0, 0.00003, 0.0])
 
 
